cd0784b0be1322ac0403239186675555a6841207
IqboljonAbdumalikov <iqboljonsecond@gmail.com>
2021-06-29 17:20:12 +0500

    Add extension title

--- js/main.js
+++ js/main.js
@@ -23,3 +23,22 @@ if (elsTabsFeaturesLink.length > 0) {
     });
   });
 }
+
+
+
+
+
+var acc = document.getElementsByClassName("questions__item");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
